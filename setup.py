7fbf943e1c80e554347c141219814c0a94c46f90
Bernát Gábor <bgabor8@bloomberg.net>
2020-10-11 09:57:37 +0100

    Move to Github Actions and add python3.9 (#27)
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 """Handle the package management of the module"""
 from setuptools import setup
 
-setup(
-    use_scm_version={
-        "write_to": "src/retype/version.py",
-        "write_to_template": '__version__ = "{version}"',
-    }
-)
+setup()
